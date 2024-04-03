92226a41e51f30d13c533847a1928e2d21d0ddea
eduardojafeth1 <eduardojafeth1@gmail.com>
2024-04-03 15:57:55 -0600

    menu page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { sidebarComponent } from '../sidebar/sidebar.component';
 import { NavSimpleComponent } from "../nav-simple/nav-simple.component";
+import { FootComponent } from '../foot/foot.component';
 
 @Component({
     selector: 'app-menu',
     standalone: true,
     templateUrl: './menu.component.html',
     styleUrl: './menu.component.scss',
-    imports: [NavSimpleComponent, sidebarComponent]
+    imports: [NavSimpleComponent, sidebarComponent, FootComponent]
 })
 export class MenuComponent {
 
